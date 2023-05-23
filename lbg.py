e710f5f9e120a5e5eaed18af85ee45c33bdf25f9
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2023-05-23 12:52:22 +0100

    Update lbg.py

--- lbg.py
+++ lbg.py
@@ -21,12 +21,6 @@ import mimetypes
 mimetypes.add_type('text/javascript', '.js')
 
 # set up the app with listening socket for http requests and appropriate hostname
-parser = argparse.ArgumentParser()
-parser.add_argument("--PORT", default="8080")
-args = parser.parse_args()
-
-PORT = args.PORT
-HOST = 'localhost'
 
 # get app to serve static files from the public directory
 app = Flask(__name__, static_url_path=f'/', static_folder='./static')
@@ -214,6 +208,12 @@ def delete_one(_id):
 
 # module import protection
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--PORT", default="8080")
+    args = parser.parse_args()
+
+    PORT = args.PORT
+    HOST = 'localhost'
     # get app to serve
     print(f'API Listening on http://{HOST}:{PORT}')
     app.run(host=HOST, port=PORT, debug=True)
